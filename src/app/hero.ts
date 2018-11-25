61adbb19059146a7088b34c38d66a1bf8848d1c9
Tyrone Rebolledo <t.rebolledo@hotmail.com>
2018-11-26 08:43:16 +1000

    Updated

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+export class Hero {
+    id: number;
+    name: string;
+}
+export class RandomTest{
+  id: number;
+  name: string; 
+} 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Tracer' },
+  { id: 12, name: 'Widowmaker' },
+  { id: 13, name: 'Bastion' },
+  { id: 14, name: 'Winston' },
+  { id: 15, name: 'Genji' },
+  { id: 16, name: 'Mcree' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
